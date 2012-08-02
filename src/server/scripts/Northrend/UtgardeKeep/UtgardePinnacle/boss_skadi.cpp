4efb4e10e306311cb6ba00064e3bc571556e417b
Albis <albiszor@gmail.com>
2012-08-03 01:49:58 +0430

    Scripts/Utgarde Pinnacle: Use spell immunities for Skadi to prevent him dying in air in any situation.

--- src/server/scripts/Northrend/UtgardeKeep/UtgardePinnacle/boss_skadi.cpp
+++ src/server/scripts/Northrend/UtgardeKeep/UtgardePinnacle/boss_skadi.cpp
@@ -206,6 +206,7 @@ public:
             Summons.DespawnAll();
             me->SetSpeed(MOVE_FLIGHT, 3.0f);
             me->RemoveUnitMovementFlag(MOVEMENTFLAG_LEVITATING);
+            me->ApplySpellImmune(0, IMMUNITY_ID, SPELL_RAPID_FIRE, true);
 
             if ((Unit::GetCreature((*me), GraufGUID) == NULL) && !me->IsMounted())
                 me->SummonCreature(CREATURE_GRAUF, Location[0].GetPositionX(), Location[0].GetPositionY(), Location[0].GetPositionZ(), 3.0f);
@@ -286,7 +287,6 @@ public:
                 if (SpellHitCount == 0)
                     LoopCounter[1] = LoopCounter[0];
 
-                me->SetHealth(me->GetMaxHealth());
 
                 SpellHitCount++;
                 if (SpellHitCount >= 3)
